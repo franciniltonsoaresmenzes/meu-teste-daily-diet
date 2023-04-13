619b28d988d40106712b5251de7573a08a22a77e
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-04-12 22:18:56 -0300

    teste para não permitir criar usuários com o mesmo e-mail.

--- test/usecases/register-user/repository/in-memory-user-repository.spec.ts
+++ test/usecases/register-user/repository/in-memory-user-repository.spec.ts
@@ -19,12 +19,31 @@ describe('In Memory user repository', () => {
     expect(user).toBeTruthy()
   })
 
-  it('should return null user is not found', async () => {
+  it('should return false user is not found', async () => {
     const users: input[] = []
     const sut = new InMemoryUserRepository(users)
     const email = 'any@email.com'
 
     const user = await sut.exist(email)
     expect(user).toBeFalsy()
+  })
+
+  it('should not create users with the same email', async () => {
+    const users: input[] = [
+      {
+        id: '11313131',
+        email: 'any@email.com',
+        password: '123456',
+      },
+    ]
+    const sut = new InMemoryUserRepository(users)
+
+    const userData: UserData = {
+      email: 'any@email.com',
+      password: '123456789',
+    }
+    await sut.add(userData)
+
+    expect(sut.repository.length).toBe(1)
   })
 })
