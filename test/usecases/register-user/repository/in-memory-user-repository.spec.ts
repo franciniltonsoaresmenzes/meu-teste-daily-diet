36fb83b52cf7b1ae8bb9ec2b5eb2f4d6dd016501
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-04-12 21:56:08 -0300

    teste de  casos de uso e repositories

---
+++ test/usecases/register-user/repository/in-memory-user-repository.spec.ts
@@ -0,0 +1,30 @@
+import { describe, expect, it } from 'vitest'
+import { UserData } from '../../../../src/entities/user-data'
+import {
+  InMemoryUserRepository,
+  input,
+} from '../../../../src/usecases/register-user/repository/in-memory-user-repository'
+
+describe('In Memory user repository', () => {
+  it('should return user if in the repository', async () => {
+    const users: input[] = []
+    const sut = new InMemoryUserRepository(users)
+    const userData: UserData = {
+      email: 'any@email.com',
+      password: '123456',
+    }
+
+    await sut.add(userData)
+    const user = await sut.finUserByEmail(userData.email)
+    expect(user!.user).toEqual(userData)
+  })
+
+  it('should return null user is not found', async () => {
+    const users: input[] = []
+    const sut = new InMemoryUserRepository(users)
+    const email = 'any@email.com'
+
+    const user = await sut.finUserByEmail(email)
+    expect(user).toBeNull()
+  })
+})
